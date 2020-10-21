acaf6952a8abb1190e9819f661b78cb48104938f
dirk <dweeteling@independer.nl>
2020-10-21 15:51:07 +0200

    Minor comment change; newline at eof

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ var socketConfig = require('./config')
 var io = require('socket.io')(http, socketConfig)
 var port = process.env.PORT || 8081
 
-var rooms = {} // key=roomId; value={socketId: socketObject}
+var rooms = {} // NB! This Object looks like: key => roomId; value => {socketId: socketObject}
 var roomsCreatedAt = new WeakMap()
 var names = new WeakMap()
 var roomId
@@ -76,10 +76,7 @@ io.on('connection', (socket) => {
 	socket.on('disconnect', () => {
 		io.to(roomId).emit('system message', `${getClientName(socket)} left ${roomId}`)
 
-		// Delete the specific socket from the room:
 		delete rooms[roomId][socket.id]
-
-		// If no more users are connected, delete the room:
 		const room = rooms[roomId]
 		if (!Object.keys(room).length) {
 			delete rooms[roomId]
